4bad3217c8133e792a4c02bccccfd63146a42c51
juhonamnam <jhnamnam@gmail.com>
2025-09-22 17:50:22 +0900

    chatbot responsive ui

--- frontend/src/bookreader/components/chatbot/index.tsx
+++ frontend/src/bookreader/components/chatbot/index.tsx
@@ -2,6 +2,8 @@ import { CHAT_TYPES, type ChatType } from "@shared/chat";
 import { AssistantChatBot } from "./AssistantChatBot";
 import { CharacterChatBot } from "./CharacterChatBot";
 import { useChatBotContext } from "./context";
+import { useState } from "react";
+import sidebarUrl from "./sidebar.png";
 
 export const ChatBot = ({
   bookId,
@@ -24,33 +26,44 @@ export const ChatBot = ({
 
   const chatController = chatControllers[selectedChatIdx];
 
+  const [chatListFolded, setChatListFolded] = useState(true);
+
   return (
-    <div className="chat">
-      <div className="chat-list">
-        {chatControllers.map((chatController, index) => (
-          <div
-            key={chatController.chatKey}
-            className={`chat-list-item ${
-              selectedChatIdx === index ? "active" : ""
-            }`}
-          >
-            <button
-              className="chat-title"
-              onClick={() => setSelectedChatIdx(index)}
-              title={chatController.chatTitle}
+    <div className={`chat${chatListFolded ? " folded" : ""}`}>
+      <div className="chat-list-toggle">
+        <button onClick={() => setChatListFolded((prev) => !prev)}>
+          <img src={sidebarUrl} />
+        </button>
+      </div>
+
+      <div className="chat-list-wrapper">
+        <div className="chat-list">
+          {chatControllers.map((chatController, index) => (
+            <div
+              key={chatController.chatKey}
+              className={`chat-list-item ${
+                selectedChatIdx === index ? "active" : ""
+              }`}
             >
-              {chatController.chatTitle}
-            </button>
-            <button
-              className="delete-chat-button"
-              onClick={() => deleteChat(index)}
-            ></button>
+              <button
+                className="chat-title"
+                onClick={() => setSelectedChatIdx(index)}
+                title={chatController.chatTitle}
+              >
+                {chatController.chatTitle}
+              </button>
+              <button
+                className="delete-chat-button"
+                onClick={() => deleteChat(index)}
+              ></button>
+            </div>
+          ))}
+          <div className="new-chat-button-wrapper">
+            <button className="new-chat-button" onClick={() => newChat()} />
           </div>
-        ))}
-        <div className="new-chat-button-wrapper">
-          <button className="new-chat-button" onClick={() => newChat()} />
         </div>
       </div>
+
       <div className="chat-container">
         <div className="chat-title">
           {chatController.isNew ? (
